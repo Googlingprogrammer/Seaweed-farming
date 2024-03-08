dd6b8246a1f8a7e152858b7ad876ac6c0a2ec755
Googlingprogrammer <minhspammail@gmail.com>
2024-03-08 10:39:55 -0500

    MOVEMENT SETUP

--- Assets/Scripts/player/playerMovement.cs
+++ Assets/Scripts/player/playerMovement.cs
@@ -2,37 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float walkSpeed = 12f;
     public float runSpeed = 24f; // Double the walk speed for running
-    public float movementSpeed; // Current movement speed
-    public float jumpHeight = 3f;
-    public float gravity = 0;
-     Vector3 velocity;
-    
+    // public float jumpHeight = 3f;
+    // public float gravity = 9.8f;
+
+    Vector3 velocity;
+
     void Update()
     {
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        float y = Input.GetButtonDown("Jump") ? 1.0f : 0.0f;
+        float y = 0f;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            y = 1.0f; // Going up
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            y = -1.0f; // Going down
+        }
 
         Vector3 move = transform.right * x + transform.forward * z + transform.up * y;
 
         // Check if the player is holding the "Shift" key to run
-        movementSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
 
-        controller.Move(move * movementSpeed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        // Move the controller
+        controller.Move((move * currentSpeed + velocity) * Time.deltaTime);
     }
 }
