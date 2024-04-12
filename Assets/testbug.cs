8626d1901e7da3f1bb68543f77cef3c68e63ef66
Googlingprogrammer <94053049+Googlingprogrammer@users.noreply.github.com>
2024-04-12 01:06:44 -0400

    doing doing doing stff

---
+++ Assets/testbug.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        // Get the Animator component attached to the object
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Check if the "E" key is pressed
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Trigger the animation
+            animator.SetTrigger("roboarm_grab|Armature_001Actions");
+        }
+    }
+}
